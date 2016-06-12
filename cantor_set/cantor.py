00f53271457d56ee29ec826dec663f078983dac4
asvnpr <vega360@gmail.com>
2016-06-12 18:27:34 -0400

    lol semicolons in python what was I doing?

--- cantor_set/cantor.py
+++ cantor_set/cantor.py
@@ -1,33 +1,33 @@
 ##prints out the cantor ternary set. asks user the number of times to iterate and a char to be used
 
 def cantorSet(N, char_print, count):  ##function that prints the cantor set
-    line = "";  ##line to be printed
+    line = ""  ##line to be printed
     if (N == 0):  ##base case
-        return();
+        return()
     else:
 
         if(count == 0):  ##prints initial line where there is no whitespace and the second line
-            print char_print*3**N;
-            print char_print*((3**N)/3)," "*(((3**N)/3)-2),char_print*((3**N)/3);
+            print char_print*3**N
+            print char_print*((3**N)/3)," "*(((3**N)/3)-2),char_print*((3**N)/3)
 
         else:
-            line = char_print*3**(N-1) + ' '*3**(N-1) + char_print*3**(N-1);
+            line = char_print*3**(N-1) + ' '*3**(N-1) + char_print*3**(N-1)
             for i in range(count):
-                line+=' '*len(line) + line;
+                line+=' '*len(line) + line
 
-            print line;
-    return cantorSet(N-1,char_print,count+1);  ##recursive call
+            print line
+    return cantorSet(N-1,char_print,count+1)  ##recursive call
 
-N = int(raw_input("Enter the number of iterations: "));
-## need to find a way to check a way to check how many chars fit in a line in the current terminal. inadequate method below: 
-##too_long = True;
+N = int(raw_input("Enter the number of iterations: "))
+## need to find a way to check a way to check how many chars fit in a line in the current terminal. inadequate method below:
+##too_long = True
 ##while(too_long):  ##error checking
   ##  if(3**N > 210):  ##approx number of chars that fit in a line in my term. should fix!!
     ##    print "Number of iterations will result in a line outside of screen bounds!"
-      ##  N = int(raw_input("Please enter a smaller number of iterations: "));
+      ##  N = int(raw_input("Please enter a smaller number of iterations: "))
     ##else:
-      ##  too_long = False;
+      ##  too_long = False
 
-char_print = raw_input("Enter the char you wish to print: ");
+char_print = raw_input("Enter the char you wish to print: ")
 
-cantorSet(N,char_print, 0); ##calls recursive function
+cantorSet(N,char_print, 0) ##calls recursive function
